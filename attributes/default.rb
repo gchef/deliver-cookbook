9f57aaa6de972e85bf8f90500d3a64bfe4f52726
Gerhard Lazu <gerhard@lazu.co.uk>
2012-08-01 15:50:30 +0100

    Allow to use specific branches (or even HEAD).

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default[:deliver][:dir] = "/var/lib/deliver"
-default[:deliver][:version] = "0.5.0"
+default[:deliver][:dir]        = "/var/lib/deliver"
+default[:deliver][:version]    = "v0.5.0"
 default[:deliver][:repository] = "git://github.com/gerhard/deliver.git"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 git node[:deliver][:dir] do
   repository node[:deliver][:repository]
-  reference "v#{node[:deliver][:version]}"
+  reference "#{node[:deliver][:version]}"
   action :sync
 end
 
