314257173a1b2f4003075e21e57eac5ed1c7628a
Gerhard Lazu <gerhard@lazu.co.uk>
2012-10-23 16:03:34 +0100

    Firewall-friendly wan_up

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 install = (node[:deliver][:action] == "install")
-wan_up = `ping -c 1 -W 1 8.8.8.8`.index(/1 (?:packets )?received/)
+wan_up = `host -W 1 google.com`.index(/has address/)
 
 if install
   git node[:deliver][:dir] do
