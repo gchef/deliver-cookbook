028aee8229d6e1897becbfe2672972c2de80f822
Gerhard Lazu <gerhard@lazu.co.uk>
2013-12-17 10:40:40 +0000

    Use latest deliver

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:deliver][:action]     = "install"
 default[:deliver][:dir]        = "/usr/local/deliver"
-default[:deliver][:version]    = "v0.6.0"
+default[:deliver][:version]    = "v0.8.0"
 default[:deliver][:repository] = "git://github.com/gerhard/deliver.git"
